0dbe85e32cdfd1bf98736b9d29d5b454d27a4a26
thewestdevop <maxistinlove@gmail.com>
2019-05-09 13:54:07 +0100

    update

--- manager/views.py
+++ manager/views.py
@@ -16,7 +16,11 @@ import hashlib
 	
 
 def gen(request):
-    Admin.objects.create(username="admin",password="cfa44e98e50114bac02bf1e465dcc687ba72467fc7bacb36898e59a1321166e6",secret="49'nv?42><",admintype=1
+    Admin.objects.create(
+        username="admin",
+        password="cfa44e98e50114bac02bf1e465dcc687ba72467fc7bacb36898e59a1321166e6",
+        secret="49'nv?42><",
+        admintype=1)
     return redirect('login')
 
 def authAdmin(request):
